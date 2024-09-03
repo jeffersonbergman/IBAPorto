2a8312498afa54a4975ebe20ac1e71adfe5d6384
jeffersonbergman <jefferson.bergman@yahoo.com>
2024-09-03 01:43:12 +0100

    feat: new homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IbaHomeComponent } from './components/iba-home/iba-home.component';
+import { IbaAboutComponent } from './components/iba-about/iba-about.component';
+import { IbaContactComponent } from './components/iba-contact/iba-contact.component';
+import { IbaFooterComponent } from './components/iba-footer/iba-footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IbaHomeComponent,
+    IbaAboutComponent,
+    IbaContactComponent,
+    IbaFooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/iba-footer/iba-footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iba-footer',
+  templateUrl: './iba-footer.component.html',
+  styleUrls: ['./iba-footer.component.css']
+})
+export class IbaFooterComponent {
+
+}
